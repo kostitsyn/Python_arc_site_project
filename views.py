ed0b6112f4e3cadb421d282371d3c5ba7170e089
kostitsyn <kostitsin.a@mail.ru>
2021-02-15 13:47:09 +0400

    Второе домашнее задание

--- views.py
+++ views.py
@@ -1,23 +1,36 @@
 from framework.templates import render
 
 
-def main_view(request):
-    secret = request.get('secret_key', None)
-    language = request.get('language', 'en')
+def main_view(requests):
+    if requests['method'] == 'GET':
+        data = requests.get('data')
+        if data:
+            name = data['name']
+            surname = data['surname']
+            age = data['age']
+            print(f'Получено сообщение от {name} {surname}, возраст {age}')
+    secret = requests.get('secret_key', None)
+    language = requests.get('language', 'en')
     return '200 OK', render('index.html', secret=secret, language=language)
 
 
-def about_view(request):
+def about_view(requests):
     return '200 OK', render('about.html')
 
 
-def contacts_view(request):
+def contacts_view(requests):
+    if requests['method'] == 'POST':
+        data = requests['data']
+        theme = data['theme']
+        text = data['text']
+        email = data['email']
+        print(f'Пришло сообщение от {email} с темой {theme} и текстом {text}')
     return '200 OK', render('contacts.html')
 
 
-def catalog_view(request):
+def catalog_view(requests):
     return '200 OK', render('catalog.html')
 
 
-def other_view(request):
+def other_view(requests):
     return '200 OK', '<h1>Just another page...</h1>'
